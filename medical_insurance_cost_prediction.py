29fc65b07aad6840a448fbea8bebbf8f8404f5ed
Alina Jamshed Ali <178809907+Alina09x@users.noreply.github.com>
2026-02-11 05:32:42 -0800

    Data Collection and Analysis

--- medical_insurance_cost_prediction.py
+++ medical_insurance_cost_prediction.py
@@ -5,3 +5,18 @@ import seaborn as sns
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn import metrics
+
+
+#Data Collection and Analysis
+
+# loading the data from csv file to a Pandas DataFrame
+insurance_dataset = pd.read_csv(r'insurance.csv')
+
+# first 5 rows of the dataframe
+insurance_dataset.head()
+
+# number of rows and columns
+insurance_dataset.shape
+
+# getting some informations about the dataset
+insurance_dataset.info()
